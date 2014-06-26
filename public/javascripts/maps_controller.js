fc90ef7ee60b93a7789e0a757093132fd6b32d32
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-06-26 00:03:02 -0700

    finally getting windows to pop up

--- public/javascripts/maps_controller.js
+++ public/javascripts/maps_controller.js
@@ -1,11 +1,7 @@
 MapsController = angular.module("MapsController", []);
 
 MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($scope, $http, $resource) {
-    var onMarkerClicked = function(marker){
-        console.log("Hello there!!")
-        marker.showWindow = true;
-        console.log(marker)
-    };
+    var apiUrl = "http://doctorstats.herokuapp.com/api/v1/";
 
 
     $scope.map = {
@@ -17,21 +13,11 @@ MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($
         latitude: 37.7683,
         longitude: -122.4408
       },
-      zoom: 14,
-      dragging: true,     
-             infoWindow: {
-                coords: {
-                    latitude: 30,
-                    longitude: -89
-                },
-                show: false
-            },
-      refresh: function () {
-        $scope.map.control.refresh(origCenter);
-      }
+      zoom: 13,
+      dragging: true
     };
 
-    var apiUrl = "http://doctorstats.herokuapp.com/api/v1/";
+
 
     $scope.getAllDoctors = function() {
       var Connection = $resource(apiUrl + "/doctors.json");
@@ -42,13 +28,22 @@ MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($
             item.url = null;
             item.showWindow = false;
             console.log(item)
-            $scope.map.markers.push(item);
+            
+            item.onClick = function() {
+              console.log("opening window")
+              item.showWindow = true;
+              $scope.$apply();
+            }
+            
+            item.closeClick = function() {
+            console.log('close click - hiding window');
+            item.showWindow = false;
+            $scope.$apply();
+            }
           }
         })
+        $scope.map.markers = result;
       });
-
-      return $scope.map.markers;
-
     };
 
     $scope.getBlueShieldDoctors = function() {
@@ -59,14 +54,24 @@ MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($
             item.icon = null;
             item.url = null;
             item.showWindow = false;
-            $scope.map.markers.push(item);
+            that = this
+            
+            item.onClick = function() {
+              console.log(that)
+              item.showWindow = true;
+              $scope.$apply();
+            }
+            
+            item.closeClick = function() {
+            console.log('close click - hiding window');
+            item.showWindow = false;
+            $scope.$apply();
+            }
           }
         })
+        $scope.map.markers = result;
       });
-
-      return $scope.map.markers;
     };
-
     $scope.getBlueCrossDoctors = function() {
       var Connection = $resource(apiUrl + "/blue_cross.json");
       Connection.query().$promise.then(function (result){
@@ -75,25 +80,24 @@ MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($
             item.icon = null;
             item.url = null;
             item.showWindow = false;
-            $scope.map.markers.push(item);
+            that = item;
+            
+            item.onClick = function() {
+              console.log(that)
+              item.showWindow = true;
+              $scope.$apply();
+            }
+            
+            item.closeClick = function() {
+            console.log('close click - hiding window');
+            item.showWindow = false;
+            $scope.$apply();
+            }
           }
         })
-       _.each($scope.map.markers,function(marker){
-        console.log(marker)
-        marker.closeClick = function(){
-            marker.showWindow = false;
-            $scope.$apply();
-        };
-        marker.onClicked = function(){
-            console.log("clicked")
-            onMarkerClicked(marker);
-        };
-        });
+        $scope.map.markers = result;
       });
-
-      return $scope.map.markers;
     };
-
     $scope.getCchpDoctors = function() {
       var Connection = $resource(apiUrl + "/cchp.json");
       Connection.query().$promise.then(function (result){
@@ -102,12 +106,23 @@ MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($
             item.icon = null;
             item.url = null;
             item.showWindow = false;
-            console.log(item)
-            $scope.map.markers.push(item);
+            that = this
+            
+            item.onClick = function() {
+              console.log(this)
+              item.showWindow = true;
+              $scope.$apply();
+            }
+            
+            item.closeClick = function() {
+            console.log('close click - hiding window');
+            item.showWindow = false;
+            $scope.$apply();
+            }
           }
         })
+        $scope.map.markers = result;
       });
-      return $scope.map.markers;
     };
 
     $scope.getKaiserDoctors = function() {
@@ -118,81 +133,62 @@ MapsController.controller('mapCtrl', ["$scope", "$http", "$resource", function($
             item.icon = null;
             item.url = null;
             item.showWindow = false;
-            console.log(item)
-            $scope.map.markers.push(item);
+            
+            item.onClick = function() {
+              console.log("opening window")
+              item.showWindow = true;
+              $scope.$apply();
+            }
+            
+            item.closeClick = function() {
+            console.log('close click - hiding window');
+            item.showWindow = false;
+            $scope.$apply();
+            }
           }
         })
+        $scope.map.markers = result;
       });
-      return $scope.map.markers;
-    };
-
-    $scope.getDoctorInfo = function(doctor) {
-      console.log('hi')
-      console.log(doctor.name);
-    };
-
-    var redirectToYelp = function(doctor) {
-      $window.location.href = doctor.url;
-    };
-
-    $scope.clearMap = function() {
-      $scope.map.refresh = "true"
-    };
-
-    $scope.displayName = function(doctor){
     };
 
 
 
     $scope.markersEvents = {
-      // click: function (gMarker, eventName, doctor) {
-      //   doctor.showWindow = true;
-      //   console.log(doctor);
-      //   console.log(gMarker);
-      //   var phone = doctor.phone.replace(/[^\w\s]/gi, '');
-      //   var name = doctor.name.replace(/ /g, '+')
-      //   // alert(doctor.name + "  " + doctor.address + "   " + doctor.latitude + " " + doctor.longitude + "    " + doctor.zip_code + " " + phone);
-      //   var yelpUrl = "http://api.yelp.com/v2/search?term=" + name + "&location=" + doctor.zip_code;
-      //   var yelp2Url = "http://api.yelp.com/phone_search?phone=" + phone + "&ywsid=IDZJqj8ZCNQzMT0jC7yIFQ";
-      // },
-      mouseover: function(gMarker, eventName, doctor) {
-        console.log(this.window)
+      click: function (gMarker, eventName, marker) {
+        marker.showWindow = true;
+        console.log(marker);
+        console.log(gMarker);
+        var phone = marker.phone.replace(/[^\w\s]/gi, '');
+        var name = marker.name.replace(/ /g, '+')
+        // alert(marker.name + "  " + marker.address + "   " + marker.latitude + " " + marker.longitude + "    " + marker.zip_code + " " + phone);
+        var yelpUrl = "http://api.yelp.com/v2/search?term=" + name + "&location=" + marker.zip_code;
+        var yelp2Url = "http://api.yelp.com/phone_search?phone=" + phone + "&ywsid=IDZJqj8ZCNQzMT0jC7yIFQ";
+      },
+      mouseover: function(gMarker, eventName, marker) {
+        // console.log("This is Gmarker", gMarker)
+        // console.log("This is eventname", eventName)
+        // console.log("This is marker", marker)
+        // console.log("parent", this.$parent)
+        // console.log("this", this)
 
-        gMarker.showWindow = true;
-        var phone = doctor.phone.replace(/[^\w\s]/gi, '');
-        var name = doctor.name.replace(/ /g, '+')
-        var url = "/lookup?q=" + name + "&l=" + doctor.zip_code
+        var phone = marker.phone.replace(/[^\w\s]/gi, '');
+        var name = marker.name.replace(/ /g, '+')
+        var url = "/lookup?q=" + name + "&l=" + marker.zip_code
         var service = $resource(url);
+
         service.get().$promise.then(function (data){
           businessArray = data.businesses;
           _.each(businessArray, function (object){
-            var doctorarr = doctor.name.split(" ")
+            var markerArr = marker.name.split(" ")
             var namearr = object.name.split(" ")
-            if (doctorarr[0] === namearr[1] || doctorarr[1] === namearr[0]) {
-              doctor.icon = object.rating_img_url_small;
-              doctor.url = object.url;
-              console.log(doctor.name + doctor.url + doctor.icon)
-              return
+            if (markerArr[0] === namearr[1] || markerArr[1] === namearr[0]) {
+              marker.icon = object.rating_img_url_small;
+              marker.url = object.url;
+              console.log(marker.name + marker.url + marker.icon)
+              return marker
             }
-            return
           });
         });
       }
     };
-
-    $scope.onInsideWindowClick = function () {
-      alert("Window hit!");
-    };
-
-    $scope.refreshMap = function () {
-      $scope.map.control.refresh({latitude: 37.7683, longitude: -122.4408});
-      $scope.map.control.getGMap().setZoom(11);
-    };
-
-    var origCenter = {latitude: $scope.map.center.latitude, longitude: $scope.map.center.longitude};
-
-    $scope.doSomething = function() {
-      console.log("hi");
-    };
-
 }]);
